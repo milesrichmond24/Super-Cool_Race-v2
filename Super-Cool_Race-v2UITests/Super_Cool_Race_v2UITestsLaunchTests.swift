df5a151d4b142b145bd6f0a55ab7511015626fce
miles.richmond24 <miles.richmond24@d155.org>
2024-04-28 09:37:10 -0500

    Initial Commit

---
+++ Super-Cool_Race-v2UITests/Super_Cool_Race_v2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Super_Cool_Race_v2UITestsLaunchTests.swift
+//  Super-Cool_Race-v2UITests
+//
+//  Created by Miles Richmond on 4/28/24.
+//
+
+import XCTest
+
+final class Super_Cool_Race_v2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
